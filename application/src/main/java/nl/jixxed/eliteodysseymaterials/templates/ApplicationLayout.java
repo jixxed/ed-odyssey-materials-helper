2dc962ce01eaf41aad762a01ec964c1fdcf2901b
jixxed <jixxed@hotmail.com>
2022-01-17 18:10:18 +0100

    add tabs

--- application/src/main/java/nl/jixxed/eliteodysseymaterials/templates/ApplicationLayout.java
+++ application/src/main/java/nl/jixxed/eliteodysseymaterials/templates/ApplicationLayout.java
@@ -1,14 +1,24 @@
 package nl.jixxed.eliteodysseymaterials.templates;
 
 import javafx.application.Application;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.geometry.Side;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.layout.AnchorPane;
 import nl.jixxed.eliteodysseymaterials.helper.AnchorPaneHelper;
+import nl.jixxed.eliteodysseymaterials.service.event.AfterFontSizeSetEvent;
 import nl.jixxed.eliteodysseymaterials.service.event.ApplicationLifeCycleEvent;
 import nl.jixxed.eliteodysseymaterials.service.event.EventService;
 
 public class ApplicationLayout extends AnchorPane {
     private BottomBar bottomBar;
     private ContentArea contentArea;
+    private TabPane tabsMain;
+    private Tab odyssey;
+    private Tab horizons;
+    private final IntegerProperty fontSize = new SimpleIntegerProperty(14);
 
     public ApplicationLayout(final Application application) {
         initComponents(application);
@@ -16,7 +26,8 @@ public class ApplicationLayout extends AnchorPane {
     }
 
     private void initEventHandling() {
-        EventService.addListener(this, ApplicationLifeCycleEvent.class, applicationLifeCycleEvent -> AnchorPaneHelper.setAnchor(this.contentArea, 0.0, this.bottomBar.getHeight(), 0.0, 0.0));
+        EventService.addListener(this, AfterFontSizeSetEvent.class, fontSizeEvent -> this.fontSize.set(fontSizeEvent.getFontSize()));
+        EventService.addListener(this, ApplicationLifeCycleEvent.class, applicationLifeCycleEvent -> AnchorPaneHelper.setAnchor(this.tabsMain, 0.0, this.bottomBar.getHeight(), 0.0, 0.0));
         this.bottomBar.heightProperty().addListener(observable -> AnchorPaneHelper.setAnchor(this.contentArea, 0.0, this.bottomBar.getHeight(), 0.0, 0.0));
     }
 
@@ -25,6 +36,17 @@ public class ApplicationLayout extends AnchorPane {
         this.bottomBar = new BottomBar();
         AnchorPaneHelper.setAnchor(this.bottomBar, null, 0.0, 0.0, 0.0);
         this.contentArea = new ContentArea(application);
-        this.getChildren().addAll(this.contentArea, this.bottomBar);
+        this.odyssey = new Tab();
+        this.odyssey.setText("On foot");
+        this.odyssey.setClosable(false);
+        this.horizons = new Tab();
+        this.horizons.setText("Ships");
+        this.horizons.setClosable(false);
+        this.tabsMain = new TabPane(this.odyssey, this.horizons);
+        this.tabsMain.setSide(Side.LEFT);
+        this.tabsMain.tabMaxWidthProperty().bind(this.tabsMain.heightProperty().divide(2).subtract(this.fontSize.multiply(5.14)));
+        this.tabsMain.tabMinWidthProperty().bind(this.tabsMain.heightProperty().divide(2).subtract(this.fontSize.multiply(5.14)));
+        this.odyssey.setContent(this.contentArea);
+        this.getChildren().addAll(this.tabsMain, this.bottomBar);
     }
 }
