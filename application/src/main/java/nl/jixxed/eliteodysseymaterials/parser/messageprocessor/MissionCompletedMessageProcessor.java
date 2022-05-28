9798557406b9e3cf2b8412083d6efc9de03d6b8f
jixxed <jixxed@hotmail.com>
2022-05-28 20:25:27 +0200

    handle non horizons mission reward exception

--- application/src/main/java/nl/jixxed/eliteodysseymaterials/parser/messageprocessor/MissionCompletedMessageProcessor.java
+++ application/src/main/java/nl/jixxed/eliteodysseymaterials/parser/messageprocessor/MissionCompletedMessageProcessor.java
@@ -1,6 +1,7 @@
 package nl.jixxed.eliteodysseymaterials.parser.messageprocessor;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import lombok.extern.slf4j.Slf4j;
 import nl.jixxed.eliteodysseymaterials.enums.HorizonsMaterial;
 import nl.jixxed.eliteodysseymaterials.enums.StoragePool;
 import nl.jixxed.eliteodysseymaterials.service.StorageService;
@@ -9,17 +10,24 @@ import nl.jixxed.eliteodysseymaterials.service.event.StorageEvent;
 
 import java.util.Iterator;
 
+@Slf4j
 public class MissionCompletedMessageProcessor implements MessageProcessor {
 
     @Override
     public void process(final JsonNode journalMessage) {
         final Iterator<JsonNode> materials = journalMessage.get("MaterialsReward").elements();
         materials.forEachRemaining(jsonNode -> {
-            final HorizonsMaterial horizonsMaterial = HorizonsMaterial.subtypeForName(jsonNode.get("Name").asText());
-            if (!horizonsMaterial.isUnknown()) {
-                StorageService.addMaterial(horizonsMaterial, jsonNode.get("Count").asInt());
+            final String name = jsonNode.get("Name").asText();
+            try {
+                final HorizonsMaterial horizonsMaterial = HorizonsMaterial.subtypeForName(name);
+                if (!horizonsMaterial.isUnknown()) {
+                    StorageService.addMaterial(horizonsMaterial, jsonNode.get("Count").asInt());
+                }
+                EventService.publish(new StorageEvent(StoragePool.SHIP));
+            } catch (final IllegalArgumentException ex) {
+                //not a horizons material reward
+                log.warn("Material was not a Horizons material: " + name);
             }
         });
-        EventService.publish(new StorageEvent(StoragePool.SHIP));
     }
 }
