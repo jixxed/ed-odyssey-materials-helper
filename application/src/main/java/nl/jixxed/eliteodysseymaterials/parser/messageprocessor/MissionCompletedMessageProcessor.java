79c9ec2f13e825387038e027bca4b0b75a07cfec
jixxed <jixxed@hotmail.com>
2022-10-19 09:51:23 +0200

    handle exception for no MaterialsReward missions

--- application/src/main/java/nl/jixxed/eliteodysseymaterials/parser/messageprocessor/MissionCompletedMessageProcessor.java
+++ application/src/main/java/nl/jixxed/eliteodysseymaterials/parser/messageprocessor/MissionCompletedMessageProcessor.java
@@ -16,22 +16,24 @@ public class MissionCompletedMessageProcessor implements MessageProcessor {
 
     @Override
     public void process(final JsonNode journalMessage) {
-        final Iterator<JsonNode> materials = journalMessage.get("MaterialsReward").elements();
-        materials.forEachRemaining(jsonNode -> {
-            final String name = jsonNode.get("Name").asText();
-            try {
-                final HorizonsMaterial horizonsMaterial = HorizonsMaterial.subtypeForName(name);
-                if (horizonsMaterial instanceof Commodity commodity && !horizonsMaterial.isUnknown()) {
-                    StorageService.addCommodity(commodity, StoragePool.SHIP, jsonNode.get("Count").asInt());
+        if (journalMessage.has("MaterialsReward")) {
+            final Iterator<JsonNode> materials = journalMessage.get("MaterialsReward").elements();
+            materials.forEachRemaining(jsonNode -> {
+                final String name = jsonNode.get("Name").asText();
+                try {
+                    final HorizonsMaterial horizonsMaterial = HorizonsMaterial.subtypeForName(name);
+                    if (horizonsMaterial instanceof Commodity commodity && !horizonsMaterial.isUnknown()) {
+                        StorageService.addCommodity(commodity, StoragePool.SHIP, jsonNode.get("Count").asInt());
+                    }
+                    if (!horizonsMaterial.isUnknown()) {
+                        StorageService.addMaterial(horizonsMaterial, jsonNode.get("Count").asInt());
+                    }
+                    EventService.publish(new StorageEvent(StoragePool.SHIP));
+                } catch (final IllegalArgumentException ex) {
+                    //not a horizons material reward
+                    log.warn("Material was not a Horizons material: " + name);
                 }
-                if (!horizonsMaterial.isUnknown()) {
-                    StorageService.addMaterial(horizonsMaterial, jsonNode.get("Count").asInt());
-                }
-                EventService.publish(new StorageEvent(StoragePool.SHIP));
-            } catch (final IllegalArgumentException ex) {
-                //not a horizons material reward
-                log.warn("Material was not a Horizons material: " + name);
-            }
-        });
+            });
+        }
     }
 }
