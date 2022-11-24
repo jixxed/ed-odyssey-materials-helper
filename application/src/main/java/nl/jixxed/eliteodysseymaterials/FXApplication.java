d86d1571432065b994d67e46239a663c085a5f97
jixxed <jixxed@hotmail.com>
2022-11-25 00:09:45 +0100

    move fleetcarrier data for legacy account to separate folder

--- application/src/main/java/nl/jixxed/eliteodysseymaterials/FXApplication.java
+++ application/src/main/java/nl/jixxed/eliteodysseymaterials/FXApplication.java
@@ -199,7 +199,8 @@ public class FXApplication extends Application {
     private void setupFleetCarrierWatcher(final File watchedFolder, final Commander commander) {
 
         if (commander != null) {
-            final File watchedFolderFleetCarrier = new File(OsConstants.CONFIG_DIRECTORY + "\\" + commander.getFid().toLowerCase(Locale.ENGLISH));
+            final String pathname = OsConstants.CONFIG_DIRECTORY + "\\" + commander.getFid().toLowerCase(Locale.ENGLISH) + "" + (commander.getGameVersion().equals(GameVersion.LEGACY) ? ".legacy" : "");
+            final File watchedFolderFleetCarrier = new File(pathname);
             if (!watchedFolderFleetCarrier.exists()) {
                 watchedFolderFleetCarrier.mkdirs();
             }
