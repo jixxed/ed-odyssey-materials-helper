1d4c52b88495709e22bdb686048ea3384b802913
jixxed <jixxed@jixxed.nl>
2023-11-30 19:25:41 +0100

    fix older codexentries being sent to eddn with bad data due to being out of sync with status.json

--- application/src/main/java/nl/jixxed/eliteodysseymaterials/parser/messageprocessor/CodexEntryMessageProcessor.java
+++ application/src/main/java/nl/jixxed/eliteodysseymaterials/parser/messageprocessor/CodexEntryMessageProcessor.java
@@ -2,11 +2,28 @@ package nl.jixxed.eliteodysseymaterials.parser.messageprocessor;
 
 import nl.jixxed.eliteodysseymaterials.schemas.journal.CodexEntry.CodexEntry;
 import nl.jixxed.eliteodysseymaterials.service.EDDNService;
+import nl.jixxed.eliteodysseymaterials.service.event.EventListener;
+import nl.jixxed.eliteodysseymaterials.service.event.EventService;
+import nl.jixxed.eliteodysseymaterials.service.event.JournalInitEvent;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CodexEntryMessageProcessor implements MessageProcessor<CodexEntry> {
+    private boolean isInitialised = Boolean.FALSE;
+    private final List<EventListener<?>> eventListeners = new ArrayList<>();
+
+    public CodexEntryMessageProcessor() {
+        eventListeners.add(EventService.addListener(this, JournalInitEvent.class, journalInitEvent ->
+                this.isInitialised = journalInitEvent.isInitialised()
+        ));
+    }
+
     @Override
     public void process(final CodexEntry event) {
-        EDDNService.codexEntry(event);
+        if(isInitialised) {
+            EDDNService.codexEntry(event);
+        }
     }
 
     @Override
