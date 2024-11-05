3f9491e797cb132e442e1c9c680a0d55a601759b
jixxed <jixxed@jixxed.nl>
2024-11-05 22:25:54 +0100

    show rank decal as (un)locked fill progressbar when final rewardrank is achieved

--- application/src/main/java/nl/jixxed/eliteodysseymaterials/templates/horizons/powerplay/PowerplayPerk.java
+++ application/src/main/java/nl/jixxed/eliteodysseymaterials/templates/horizons/powerplay/PowerplayPerk.java
@@ -1,5 +1,6 @@
 package nl.jixxed.eliteodysseymaterials.templates.horizons.powerplay;
 
+import javafx.beans.binding.StringBinding;
 import javafx.geometry.Orientation;
 import javafx.scene.control.Label;
 import javafx.scene.control.Separator;
@@ -57,19 +58,18 @@ public class PowerplayPerk extends VBox implements Template {
                 .withStyleClass("power-category")
                 .withText(LocaleService.getStringBinding(this.perk.getLocalizationTitleKey()))
                 .build();
-//        Label subtitle = LabelBuilder.builder()
-//                .withStyleClass("power-category-explain")
-//                .withText(LocaleService.getStringBinding(this.perk.getLocalizationKey(), this.rewards.getLast().reward(), this.rewards.stream().map(RankReward::rank).map(String::valueOf).collect(Collectors.joining(", "))))
-//                .build();
         TextFlow subtitle = HighlightTextFlowBuilder.builder()
                 .withStyleClass("power-category-explain")
                 .withHighlightStyleClass("power-category-explain-highlight")
                 .withText(this.perk.getLocalizationKey(), this.rewards.getLast().reward() + "%", this.rewards.stream().map(RankReward::rank).map(String::valueOf).collect(Collectors.joining(", ")))
                 .build();
         final Integer reward = this.rewards.stream().filter(rankReward -> rankReward.rank() <= 55).max(Comparator.comparing(RankReward::rank)).map(RankReward::reward).orElse(0);
+        final StringBinding currentProgressBinding = (PowerPerk.RANK_DECAL.equals(this.perk))
+                ? LocaleService.getStringBinding(getRankRewardStringBinding(reward))
+                : LocaleService.getStringBinding("tab.powerplay.current.reward", reward);
         currentProgress = LabelBuilder.builder()
                 .withStyleClass("power-category-current")
-                .withText(LocaleService.getStringBinding("tab.powerplay.current.reward", reward))
+                .withText(currentProgressBinding)
                 .build();
         segmentedBar = createSegmentedBar();
         this.getChildren().add(new Separator(Orientation.HORIZONTAL));
@@ -78,20 +78,27 @@ public class PowerplayPerk extends VBox implements Template {
         update();
     }
 
+    private static String getRankRewardStringBinding(Integer reward) {
+        return (reward > 0) ? "tab.powerplay.current.reward.unlocked" : "tab.powerplay.current.reward.locked";
+    }
+
     @Override
     public void initEventHandling() {
         this.eventListeners.add(EventService.addListener(true, this, PowerplayEvent.class, powerplayEvent ->
                 this.update()));
     }
 
-    public void update(){
+    public void update() {
         this.getChildren().remove(segmentedBar);
         this.getChildren().remove(currentProgress);
-        if((Power.ALL.equals(this.power) && !Power.NONE.equals(ApplicationState.getInstance().getPower())) || this.power.equals(ApplicationState.getInstance().getPower())){
+        if ((Power.ALL.equals(this.power) && !Power.NONE.equals(ApplicationState.getInstance().getPower())) || this.power.equals(ApplicationState.getInstance().getPower())) {
             segmentedBar = createSegmentedBar();
             this.getChildren().add(segmentedBar);
             final Integer reward = this.rewards.stream().filter(rankReward -> rankReward.rank() <= ApplicationState.getInstance().getPowerRank()).max(Comparator.comparing(RankReward::rank)).map(RankReward::reward).orElse(0);
-            currentProgress.textProperty().bind(LocaleService.getStringBinding("tab.powerplay.current.reward", reward));
+            final StringBinding currentProgressBinding = (PowerPerk.RANK_DECAL.equals(this.perk))
+                    ? LocaleService.getStringBinding(getRankRewardStringBinding(reward))
+                    : LocaleService.getStringBinding("tab.powerplay.current.reward", reward);
+            currentProgress.textProperty().bind(currentProgressBinding);
             this.getChildren().add(currentProgress);
         }
     }
@@ -109,6 +116,9 @@ public class PowerplayPerk extends VBox implements Template {
                         SegmentType.RANK_DONE, Color.rgb(46, 146, 223)
                 ), false));
         int unlockedRank = (int) Math.min(ApplicationState.getInstance().getPowerRank(), MAX_RANK);
+        if (unlockedRank >= this.rewards.stream().max(Comparator.comparing(RankReward::rank)).map(RankReward::rank).orElse(0)) {
+            unlockedRank = MAX_RANK;
+        }
         int currentRank = 0;
         for (RankReward rank : this.rewards) {
             if (rank.rank() <= unlockedRank) {
