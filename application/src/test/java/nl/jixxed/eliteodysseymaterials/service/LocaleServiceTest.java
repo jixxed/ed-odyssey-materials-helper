77cb7c97336d276f2bc78e4c389a786c0844833a
jixxed <jixxed@jixxed.nl>
2025-07-29 15:53:29 +0200

    fix localizations and test that checks if all localizations are present

--- application/src/test/java/nl/jixxed/eliteodysseymaterials/service/LocaleServiceTest.java
+++ application/src/test/java/nl/jixxed/eliteodysseymaterials/service/LocaleServiceTest.java
@@ -1,15 +1,22 @@
 package nl.jixxed.eliteodysseymaterials.service;
 
+import lombok.extern.slf4j.Slf4j;
+import nl.jixxed.eliteodysseymaterials.domain.ships.ShipModule;
+import nl.jixxed.eliteodysseymaterials.enums.ApplicationLocale;
 import org.junit.jupiter.api.Test;
 
 import java.util.Locale;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-
+@Slf4j
 class LocaleServiceTest {
 
     @Test
     void testAllCSVColumnsPresent() {
-        assertDoesNotThrow(() -> LocaleService.setCurrentLocale(Locale.forLanguageTag("ru")));
+        ShipModule.getBasicModules();
+        //set to last locale to ensure all locales are present
+        Locale locale = ApplicationLocale.values()[ApplicationLocale.values().length - 1].getLocale();
+        log.info("Locale = {}", locale);
+        assertDoesNotThrow(() -> LocaleService.setCurrentLocale(locale));
     }
 }
