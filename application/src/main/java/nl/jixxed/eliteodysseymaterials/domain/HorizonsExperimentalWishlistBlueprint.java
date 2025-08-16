e0488a81b3d4af32508f5f34dc717cc0c56f0d7f
jixxed <jixxed@jixxed.nl>
2025-08-16 16:44:40 +0200

    Bugfix for individual experimental effects (Missile racks)

--- application/src/main/java/nl/jixxed/eliteodysseymaterials/domain/HorizonsExperimentalWishlistBlueprint.java
+++ application/src/main/java/nl/jixxed/eliteodysseymaterials/domain/HorizonsExperimentalWishlistBlueprint.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import nl.jixxed.eliteodysseymaterials.enums.HorizonsBlueprintName;
 import nl.jixxed.eliteodysseymaterials.enums.HorizonsBlueprintType;
 
 @NoArgsConstructor
@@ -12,4 +13,23 @@ import nl.jixxed.eliteodysseymaterials.enums.HorizonsBlueprintType;
 @EqualsAndHashCode(callSuper = true)
 public final class HorizonsExperimentalWishlistBlueprint extends HorizonsWishlistBlueprint {
     private HorizonsBlueprintType blueprintType;
+
+    public void setRecipeName(HorizonsBlueprintName recipeName) {
+        super.setRecipeName(recipeName);
+        bugfix(recipeName, this.blueprintType);
+    }
+    private void bugfix(HorizonsBlueprintName recipeName, HorizonsBlueprintType blueprintType) {
+        if (blueprintType != null && HorizonsBlueprintName.MISSILE_RACK.equals(recipeName)) {
+            if (blueprintType == HorizonsBlueprintType.DRAG_MUNITION) {
+                super.setRecipeName(HorizonsBlueprintName.SEEKER_MISSILE_RACK);
+            } else {
+                super.setRecipeName(HorizonsBlueprintName.DUMBFIRE_MISSILE_RACK);
+            }
+        }
+    }
+
+    public void setBlueprintType(HorizonsBlueprintType blueprintType) {
+        this.blueprintType = blueprintType;
+        bugfix(getRecipeName(), blueprintType);
+    }
 }
