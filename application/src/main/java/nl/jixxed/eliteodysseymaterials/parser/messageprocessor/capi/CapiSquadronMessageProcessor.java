c7855bec9044ddaec32890463bc7fbafe25dcb8f
jixxed <jixxed@jixxed.nl>
2025-09-10 16:16:26 +0200

    Bugfix for when you don't own a squadron carrier

--- application/src/main/java/nl/jixxed/eliteodysseymaterials/parser/messageprocessor/capi/CapiSquadronMessageProcessor.java
+++ application/src/main/java/nl/jixxed/eliteodysseymaterials/parser/messageprocessor/capi/CapiSquadronMessageProcessor.java
@@ -13,8 +13,9 @@ import nl.jixxed.eliteodysseymaterials.service.event.EventService;
 import nl.jixxed.eliteodysseymaterials.service.event.StorageEvent;
 
 import java.math.BigInteger;
+
 @Slf4j
-public class CapiSquadronMessageProcessor implements CapiMessageProcessor<CapiSquadron>{
+public class CapiSquadronMessageProcessor implements CapiMessageProcessor<CapiSquadron> {
     private static final CarrierAssetParser ASSET_PARSER = new CarrierAssetParser();
     private static final CarrierDataParser DATA_PARSER = new CarrierDataParser();
     private static final CarrierGoodParser GOOD_PARSER = new CarrierGoodParser();
@@ -22,6 +23,7 @@ public class CapiSquadronMessageProcessor implements CapiMessageProcessor<CapiSq
     private static final CarrierBankParser BANK_PARSER = new CarrierBankParser();
     private static final CarrierBuyOrderParser BUY_ORDER_PARSER = new CarrierBuyOrderParser();
     private static final CarrierSellOrderParser SELL_ORDER_PARSER = new CarrierSellOrderParser();
+
     @Override
     public void process(CapiSquadron data) {
         SquadronPerk primaryPerk = SquadronPerk.forId(data.getPerks().getPrimary());
@@ -31,17 +33,19 @@ public class CapiSquadronMessageProcessor implements CapiMessageProcessor<CapiSq
 
         StorageService.resetSquadronCarrierCounts();
 //        OrderService.clearOrders();
-
-        SELL_ORDER_PARSER.parseOdyssey(data.getSquadronCarrier().getOrders().getOnfootmicroresources().getSales(), StoragePool.SQUADRONCARRIER);
-        BUY_ORDER_PARSER.parseOdyssey(data.getSquadronCarrier().getOrders().getOnfootmicroresources().getPurchases(), StoragePool.SQUADRONCARRIER);
+        data.getSquadronCarrier().ifPresent(squadronCarrier -> {
+            SELL_ORDER_PARSER.parseOdyssey(squadronCarrier.getOrders().getOnfootmicroresources().getSales(), StoragePool.SQUADRONCARRIER);
+            BUY_ORDER_PARSER.parseOdyssey(squadronCarrier.getOrders().getOnfootmicroresources().getPurchases(), StoragePool.SQUADRONCARRIER);
 //        SELL_ORDER_PARSER.parse(data.getSquadronCarrier().getOrders().getCommodities().getSales());
 //        BUY_ORDER_PARSER.parse(data.getSquadronCarrier().getOrders().getCommodities().getPurchases());
 
-        ASSET_PARSER.parse(data.getSquadronCarrier().getCarrierLocker().getAssets(), StoragePool.SQUADRONCARRIER);
-        GOOD_PARSER.parse(data.getSquadronCarrier().getCarrierLocker().getGoods(), StoragePool.SQUADRONCARRIER);
-        DATA_PARSER.parse(data.getSquadronCarrier().getCarrierLocker().getData(), StoragePool.SQUADRONCARRIER);
+            ASSET_PARSER.parse(squadronCarrier.getCarrierLocker().getAssets(), StoragePool.SQUADRONCARRIER);
+            GOOD_PARSER.parse(squadronCarrier.getCarrierLocker().getGoods(), StoragePool.SQUADRONCARRIER);
+            DATA_PARSER.parse(squadronCarrier.getCarrierLocker().getData(), StoragePool.SQUADRONCARRIER);
 
-        COMMODITY_PARSER.parse(data.getSquadronCarrier().getCargo(), StoragePool.SQUADRONCARRIER);
+            COMMODITY_PARSER.parse(squadronCarrier.getCargo(), StoragePool.SQUADRONCARRIER);
+        });
+        //this probably still requires a carrier?
         BANK_PARSER.parse(data.getBank(), StoragePool.SQUADRONCARRIER);
         //currently there is no market on the carrier
 //        try {
