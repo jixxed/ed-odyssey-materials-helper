a6b34f34ae9e604bbfa851485b75ffc6b4322964
jixxed <jixxed@jixxed.nl>
2026-02-24 18:42:33 +0100

    fix Agile Thrusters optimal mass

--- application/src/main/java/nl/jixxed/eliteodysseymaterials/templates/horizons/shipbuilder/stats/ModuleProfile.java
+++ application/src/main/java/nl/jixxed/eliteodysseymaterials/templates/horizons/shipbuilder/stats/ModuleProfile.java
@@ -4,14 +4,6 @@ import static java.lang.Math.pow;
 
 public record ModuleProfile(Double minimumMass, Double optimalMass, Double maximumMass, Double minimumMultiplier,
                             Double optimalMultiplier, Double maximumMultiplier) {
-    public ModuleProfile(Double minimumMass, Double optimalMass, Double maximumMass, Double minimumMultiplier, Double optimalMultiplier, Double maximumMultiplier) {
-        this.minimumMass = minimumMass;
-        this.optimalMass = minimumMass.equals(optimalMass) ? maximumMass - minimumMass : optimalMass;
-        this.maximumMass = maximumMass;
-        this.minimumMultiplier = minimumMultiplier;
-        this.optimalMultiplier = optimalMultiplier;
-        this.maximumMultiplier = maximumMultiplier;
-    }
 
     public double getMassCurveMultiplier(final double mass) {
         if(maximumMultiplier.equals(minimumMultiplier)){
