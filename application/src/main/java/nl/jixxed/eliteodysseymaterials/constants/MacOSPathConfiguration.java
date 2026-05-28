fdee8af6d5e166d6d30c0cf6cd960704bfbe0b8d
stefanlight8 <64615032+stefanlight8@users.noreply.github.com>
2026-05-28 23:33:46 +0300

    macOS: Move config from `./config/ed-odyssey-materials-helper` to `/Library/Application Support/Elite Dangerous Odyssey Materials Helper`

--- application/src/main/java/nl/jixxed/eliteodysseymaterials/constants/MacOSPathConfiguration.java
+++ application/src/main/java/nl/jixxed/eliteodysseymaterials/constants/MacOSPathConfiguration.java
@@ -11,13 +11,16 @@
 package nl.jixxed.eliteodysseymaterials.constants;
 
 public class MacOSPathConfiguration implements PathConfiguration {
+
     private static final String CONFIG_DIRECTORY;
     private static final String DEFAULT_WATCHED_FOLDER;
     private static final String OS_SLASH;
 
     static {
         String userHome = System.getProperty("user.home");
-        CONFIG_DIRECTORY = userHome + "/.config/odyssey-materials-helper";
+        CONFIG_DIRECTORY =
+            userHome +
+            "/Library/Application Support/Elite Dangerous Odyssey Materials Helper/";
         DEFAULT_WATCHED_FOLDER = userHome + "/configure/your/journal/folder";
         OS_SLASH = "/";
     }
