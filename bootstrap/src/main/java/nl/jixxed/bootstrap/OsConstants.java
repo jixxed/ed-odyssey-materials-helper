9a299ddd4e5195943af11040dff9ae459e23fbb5
stefanlight8 <64615032+stefanlight8@users.noreply.github.com>
2026-05-29 17:44:13 +0300

    macOS: add macOS paths and constants

--- bootstrap/src/main/java/nl/jixxed/bootstrap/OsConstants.java
+++ bootstrap/src/main/java/nl/jixxed/bootstrap/OsConstants.java
@@ -24,7 +24,7 @@ class OsConstants {
             );
             case Linux -> setLinux();
             case Windows -> setWindows();
-            case MacOS -> setMacOs();
+            case MacOS -> setMacOs("aarch64"); // TODO: determine arch
         }
     }
 
@@ -49,8 +49,8 @@ class OsConstants {
             "%s/lib/app/Elite Dangerous Odyssey Materials Helper.cfg";
     }
 
-    private static void setMacOs() {
-        UPDATE_FILE_SUFFIX = ".macos.zip";
+    private static void setMacOs(String arch) {
+        UPDATE_FILE_SUFFIX = ".portable.macos.zip";
         KILL_COMMAND = "pkill -f \"Elite Dangerous Odyssey Materials Helper\"";
         START_COMMAND =
             "open \"%s/Elite Dangerous Odyssey Materials Helper.app\"";
