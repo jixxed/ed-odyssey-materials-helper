43ca46e7fc1c9e7b940c3e6af059eb6feab4fd3c
jixxed <jixxed@jixxed.nl>
2026-06-17 12:51:05 +0200

    remove paypal

--- application/src/main/java/nl/jixxed/eliteodysseymaterials/templates/generic/menu/About.java
+++ application/src/main/java/nl/jixxed/eliteodysseymaterials/templates/generic/menu/About.java
@@ -10,10 +10,12 @@
 
 package nl.jixxed.eliteodysseymaterials.templates.generic.menu;
 
-import javafx.util.Duration;
 import lombok.extern.slf4j.Slf4j;
 import nl.jixxed.eliteodysseymaterials.FXApplication;
-import nl.jixxed.eliteodysseymaterials.builder.*;
+import nl.jixxed.eliteodysseymaterials.builder.BoxBuilder;
+import nl.jixxed.eliteodysseymaterials.builder.HyperlinkBuilder;
+import nl.jixxed.eliteodysseymaterials.builder.LabelBuilder;
+import nl.jixxed.eliteodysseymaterials.builder.ResizableImageViewBuilder;
 import nl.jixxed.eliteodysseymaterials.service.LocaleService;
 import nl.jixxed.eliteodysseymaterials.service.PinnedBlueprintService;
 import nl.jixxed.eliteodysseymaterials.service.VersionService;
@@ -25,21 +27,9 @@ import java.io.IOException;
 @Slf4j
 public
 class About extends DestroyableVBox implements DestroyableTemplate {
-    private static DestroyableLabel commissionPP;
-    private static DestroyableLabel commissionGH;
-    private static DestroyableTooltip tooltipPP;
-    private static DestroyableTooltip tooltipGH;
     private final String betaVersion = "2.50-beta11";
     private DestroyableLabel versionLabel;
-    private DestroyableHyperlink link;
-    private DestroyableHyperlink linkTutorial;
-    private DestroyableHyperlink linkPinPlanner;
-    private DestroyableHyperlink donate;
-    private DestroyableHyperlink donate2;
-    private DestroyableHyperlink discord;
-    private DestroyableResizableImageView donateImage;
-    private DestroyableResizableImageView donate2Image;
-    private DestroyableResizableImageView discordImage;
+    private DestroyableHyperlink downloadLink;
 
     public About() {
         initComponents();
@@ -48,105 +38,114 @@ class About extends DestroyableVBox implements DestroyableTemplate {
 
     public void initComponents() {
         this.getStyleClass().add("about");
-        this.versionLabel = LabelBuilder.builder()
-                .withStyleClass("about-version")
-                .build();
-        this.link = HyperlinkBuilder.builder()
-                .withStyleClass("about-download-link")
-                .withText(LocaleService.getStringBinding("menu.about.download"))
-                .withOnAction(actionEvent ->
-                        FXApplication.getInstance().getHostServices().showDocument("https://github.com/jixxed/ed-odyssey-materials-helper/releases"))
-                .build();
-        this.linkTutorial = HyperlinkBuilder.builder()
-                .withStyleClass("about-download-link")
-                .withText(LocaleService.getStringBinding("menu.about.tutorial"))
-                .withOnAction(actionEvent ->
-                        FXApplication.getInstance().getHostServices().showDocument("https://www.youtube.com/playlist?list=PLg3S2KmbzGpPvHqMJrP2yFKEyUr8Axn0T"))
-                .build();
-        this.linkPinPlanner = HyperlinkBuilder.builder()
-                .withStyleClass("about-download-link")
-                .withText(LocaleService.getStringBinding("menu.about.pinplanner"))
-                .withOnAction(actionEvent ->
-                        FXApplication.getInstance().getHostServices().showDocument("https://pinplanner.edomh.nl/?config=" + PinnedBlueprintService.getPinConfigForUrl()))
-                .build();
-        this.donateImage = ResizableImageViewBuilder.builder()
-                .withStyleClass("about-donate-image")
-                .withImage("/images/paypal.png")
-                .build();
-        this.donate2Image = ResizableImageViewBuilder.builder()
-                .withStyleClass("about-donate-image")
-                .withImage("/images/github_sponsors.png")
-                .build();
-        this.discordImage = ResizableImageViewBuilder.builder()
-                .withStyleClass("about-discord-image")
-                .withImage("/images/other/discord_button.png")
-                .build();
-        this.donate = HyperlinkBuilder.builder()
-                .withStyleClass("about-donate-button")
-                .withOnAction(actionEvent ->
-                        FXApplication.getInstance().getHostServices().showDocument("https://www.paypal.com/donate?business=4LB2HUSB7NDAS&item_name=Odyssey+Materials+Helper"))
-                .withGraphic(this.donateImage)
-                .build();
-        this.donate2 = HyperlinkBuilder.builder()
-                .withStyleClass("about-donate-button")
-                .withOnAction(actionEvent ->
-                        FXApplication.getInstance().getHostServices().showDocument("https://github.com/sponsors/jixxed"))
-                .withGraphic(this.donate2Image)
-                .build();
-        this.discord = HyperlinkBuilder.builder()
-                .withStyleClass("about-discord")
-                .withOnAction(actionEvent ->
-                        FXApplication.getInstance().getHostServices().showDocument("https://discord.gg/M8Rgz4AmmA"))
-                .withGraphic(this.discordImage)
-                .build();
 
         this.getNodes().addAll(
-                BoxBuilder.builder().withNodes(new GrowingRegion(), this.versionLabel, new GrowingRegion()).buildHBox(),
-                BoxBuilder.builder().withNodes(new GrowingRegion(), this.link, new GrowingRegion()).buildHBox(),
-                donateButtons(),
-                donateLabels(),
-                BoxBuilder.builder().withNodes(new GrowingRegion(), this.discord, new GrowingRegion()).buildHBox(),
-                BoxBuilder.builder().withNodes(new GrowingRegion(), this.linkTutorial, new GrowingRegion()).buildHBox(),
-                BoxBuilder.builder().withNodes(new GrowingRegion(), this.linkPinPlanner, new GrowingRegion()).buildHBox()
+                version(),
+                downloadLink(),
+                donateTitle(),
+                donateButton(),
+                donateLabel(),
+                discord(),
+                tutorial(),
+                pinPlanner()
         );
     }
 
-    private static DestroyableHBox donateLabels() {
-        commissionPP = LabelBuilder.builder()
-                .withStyleClass("about-donate-commission")
-                .withNonLocalizedText("2.89%+39ct")
+    private DestroyableHBox pinPlanner() {
+        DestroyableHyperlink linkPinPlanner = HyperlinkBuilder.builder()
+                .withStyleClass("about-download-link")
+                .withText(LocaleService.getStringBinding("menu.about.pinplanner"))
+                .withOnAction(_ ->
+                        FXApplication.getInstance().getHostServices().showDocument("https://pinplanner.edomh.nl/?config=" + PinnedBlueprintService.getPinConfigForUrl()))
                 .build();
-        commissionGH = LabelBuilder.builder()
-                .withStyleClass("about-donate-commission")
-                .withNonLocalizedText("0%")
+        return BoxBuilder.builder().withNodes(new GrowingRegion(), linkPinPlanner, new GrowingRegion()).buildHBox();
+    }
+
+    private DestroyableHBox tutorial() {
+        DestroyableHyperlink linkTutorial = HyperlinkBuilder.builder()
+                .withStyleClass("about-download-link")
+                .withText(LocaleService.getStringBinding("menu.about.tutorial"))
+                .withOnAction(_ ->
+                        FXApplication.getInstance().getHostServices().showDocument("https://www.youtube.com/playlist?list=PLg3S2KmbzGpPvHqMJrP2yFKEyUr8Axn0T"))
                 .build();
-        tooltipPP = TooltipBuilder.builder()
-                .withShowDelay(Duration.ZERO)
-                .withText("menu.about.commission")
+        return BoxBuilder.builder().withNodes(new GrowingRegion(), linkTutorial, new GrowingRegion()).buildHBox();
+    }
+
+    private DestroyableHBox discord() {
+        DestroyableResizableImageView discordImage = ResizableImageViewBuilder.builder()
+                .withStyleClass("about-discord-image")
+                .withImage("/images/other/discord_button.png")
                 .build();
-        tooltipGH = TooltipBuilder.builder()
-                .withShowDelay(Duration.ZERO)
-                .withText("menu.about.commission")
+        DestroyableHyperlink discord = HyperlinkBuilder.builder()
+                .withStyleClass("about-discord")
+                .withOnAction(_ ->
+                        FXApplication.getInstance().getHostServices().showDocument("https://discord.gg/M8Rgz4AmmA"))
+                .withGraphic(discordImage)
                 .build();
-        tooltipPP.install(commissionPP);
-        tooltipGH.install(commissionGH);
+        return BoxBuilder.builder().withNodes(new GrowingRegion(), discord, new GrowingRegion()).buildHBox();
+    }
+
+    private DestroyableHBox downloadLink() {
+        this.downloadLink = HyperlinkBuilder.builder()
+                .withStyleClass("about-download-link")
+                .withText(LocaleService.getStringBinding("menu.about.download"))
+                .withOnAction(_ ->
+                        FXApplication.getInstance().getHostServices().showDocument("https://github.com/jixxed/ed-odyssey-materials-helper/releases"))
+                .build();
+        return BoxBuilder.builder().withNodes(new GrowingRegion(), this.downloadLink, new GrowingRegion()).buildHBox();
+    }
+
+    private DestroyableHBox version() {
+        this.versionLabel = LabelBuilder.builder()
+                .withStyleClass("about-version")
+                .build();
+        return BoxBuilder.builder().withNodes(new GrowingRegion(), this.versionLabel, new GrowingRegion()).buildHBox();
+    }
+
+    private DestroyableHBox donateTitle() {
+        DestroyableLabel donate = LabelBuilder.builder()
+                .withText("menu.about.sponsor.github")
+                .build();
         return BoxBuilder.builder()
                 .withStyleClass("about-donate")
                 .withNodes(
                         new GrowingRegion(),
-                        commissionPP,
+                        donate,
+                        new GrowingRegion()
+
+                ).buildHBox();
+    }
+
+    private DestroyableHBox donateLabel() {
+        DestroyableLabel commissionGH = LabelBuilder.builder()
+                .withStyleClass("about-donate-commission")
+                .withText("menu.about.sponsor.github.commission")
+                .build();
+        return BoxBuilder.builder()
+                .withStyleClass("about-donate")
+                .withNodes(
+                        new GrowingRegion(),
                         commissionGH,
                         new GrowingRegion()
                 ).buildHBox();
     }
 
-    private DestroyableHBox donateButtons() {
+    private DestroyableHBox donateButton() {
+        DestroyableResizableImageView donateImage = ResizableImageViewBuilder.builder()
+                .withStyleClass("about-donate-image")
+                .withImage("/images/github_sponsors.png")
+                .build();
+        DestroyableHyperlink donate = HyperlinkBuilder.builder()
+                .withStyleClass("about-donate-button")
+                .withOnAction(_ ->
+                        FXApplication.getInstance().getHostServices().showDocument("https://github.com/sponsors/jixxed"))
+                .withGraphic(donateImage)
+                .build();
         return BoxBuilder.builder()
                 .withStyleClass("about-donate")
                 .withNodes(
                         new GrowingRegion(),
-                        this.donate,
-                        this.donate2,
+                        donate,
                         new GrowingRegion()
 
                 ).buildHBox();
@@ -166,20 +165,13 @@ class About extends DestroyableVBox implements DestroyableTemplate {
                 this.versionLabel.addBinding(this.versionLabel.textProperty(), LocaleService.getStringBinding("menu.about.version", "dev"));
             } else if (buildVersion.equals(latestVersion)) {
                 this.versionLabel.addBinding(this.versionLabel.textProperty(), LocaleService.getStringBinding("menu.about.version", buildVersion));
-                this.link.setVisible(false);
+                this.downloadLink.setVisible(false);
             } else {
                 this.versionLabel.addBinding(this.versionLabel.textProperty(), LocaleService.getStringBinding("menu.about.version.new", buildVersion, latestVersion));
             }
         } else {
             this.versionLabel.setText(this.betaVersion);
-            this.link.setVisible(false);
+            this.downloadLink.setVisible(false);
         }
-    }
-
-    @Override
-    public void destroyInternal() {
-        super.destroyInternal();
-//        Tooltip.uninstall(commissionPP, tooltipPP);
-//        Tooltip.uninstall(commissionGH, tooltipGH);
     }
 }
