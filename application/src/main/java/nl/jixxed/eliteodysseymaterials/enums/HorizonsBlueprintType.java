41234392434f95afd7f8667c089d534f955570c8
jixxed <jixxed@jixxed.nl>
2026-07-21 17:09:41 +0200

    remove MERC_SUPPORT_FOCUSED_POWER_DISTRIBUTOR_3D reporting

--- application/src/main/java/nl/jixxed/eliteodysseymaterials/enums/HorizonsBlueprintType.java
+++ application/src/main/java/nl/jixxed/eliteodysseymaterials/enums/HorizonsBlueprintType.java
@@ -979,7 +979,6 @@ public enum HorizonsBlueprintType {
 
     public boolean toReport() {
         return MERC_SUPPORT_FOCUSED_POWER_DISTRIBUTOR_3A.equals(this)
-                || MERC_SUPPORT_FOCUSED_POWER_DISTRIBUTOR_3D.equals(this)
                 || MERC_SUPPORT_FOCUSED_POWER_DISTRIBUTOR_4A.equals(this)
                 || MERC_DOUBLE_SCREAMING_FRAGMENT_CANNON_1EG.equals(this)
                 || MERC_LOCKDOWN_SEEKER_MISSILE_RACK_2B.equals(this);
